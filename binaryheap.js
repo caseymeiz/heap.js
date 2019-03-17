5fef00d626200c32c46e1be8dc658c7c680eff66
Casey Meisenzahl <cjm6121@cs.rit.edu>
2019-03-16 22:52:51 -0400

    :construction: add parameters and index lookup helpers

--- binaryheap.js
+++ binaryheap.js
@@ -1,24 +1,57 @@
 module.exports = function () {
-    function BinaryHeap (comparitor, complete_tree) {
-        this.comparitor = comparitor;
-        this.heap = complete_tree;
+
+    function BinaryHeap (complete_tree, compareFunction) {
+        this.tree = complete_tree || defaults.tree;
+        this.compareFunction = compareFunction || defaults.compareFunction;
     }
 
     BinaryHeap.prototype = {
-        constructor: BinaryHeap,
+        constructor : BinaryHeap,
 
-        decreaseKey : function () {},
+        decreaseKey : function (index, new_key) {},
 
-        delete : function () {},
+        delete : function (index) {},
 
-        extractMin : function (),
+        extractMin : function () {},
 
-        insert : function () {},
+        insert : function (element) {},
 
         union : function () {}
     }
 
+    var defaults = {
+        tree : [],
 
+        compareFunction : function (firstEl, secondEl) {
+            return secondEl - firstEl;
+        }
+    }
+
+    function siftUp (heap, index) {
+
+    }
+
+    function siftDown (haep, index) {
+
+    }
+
+    function parentIndex (currentIndex) {
+        return (currentIndex)/2;
+    }
+
+    function rightIndex(currentIndex) {
+        return 2*currentIndex + 1;
+    }
+
+    function leftIndex(currentIndex) {
+        return 2*(currentIndex - 1) +1
+    }
+
+    function buildHeap (tree) {
+        for (var i = Math.floor(tree.length/2); i >= 1; i--) {
+            sift_down(tree, i);
+        }
+    }
 
     return BinaryHeap
 }()
