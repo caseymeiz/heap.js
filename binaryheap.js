bc0d464b24783c6a6ca747791d800455483b5ae0
Casey Meisenzahl <cjm6121@cs.rit.edu>
2019-03-28 01:58:27 -0400

    :sparkles: heap sort and build heap

--- binaryheap.js
+++ binaryheap.js
@@ -8,8 +8,13 @@ module.exports = function () {
     }
 
     function BinaryHeap (complete_tree, compareFunction) {
-        this.tree = complete_tree || defaults.tree;
         this.compareFn = compareFunction || defaults.compareFunction;
+        this.size = 0;
+        if (complete_tree) {
+            this.buildHeap(complete_tree)
+        } else {
+            this.tree = defaults.tree;
+        }
     }
 
     BinaryHeap.prototype = {
@@ -19,22 +24,49 @@ module.exports = function () {
 
         delete : function (index) {},
 
-        extractMin : function () {},
+        extractMin : function () {
+            if (this.size === 0) {
+                return null;
+            }
 
-        insert : function (element) {},
+            var min = this.tree[0];
+            this.size = this.size - 1;
+
+            if (this.size === 0) {
+                return min;
+            }
+
+            this.tree[0] = this.tree[this.size];
+
+            this.tree[this.size] = min;
+
+            this.siftDown(this, 0);
+
+            return min;
+        },
+
+        insert : function (element) {
+            this.tree[this.size] = element;
+            this.size = this.size + 1;
+            this.siftUp(this, this.size - 1);
+        },
 
         union : function () {},
 
         siftDown : function (heap, index) {
-            var left_child = leftChild(index);
-            var right_child = rightChild(index);
+            var left_child = leftChildIndex(index);
+            var right_child = rightChildIndex(index);
             var follow = index;
 
-            if (heap.size > left_child && heap.compareFn(heap.tree[follow], heap.tree[left_child]) > 0) {
-                follow = left_index;
+            // console.log('index ' + index + ' -> ' +heap.tree[index]);
+            // console.log('left ' +left_child +' -> '+heap.tree[left_child]);
+            // console.log('right ' +right_child +' -> '+heap.tree[right_child]);
+
+            if (heap.size > left_child && heap.compareFn(heap.tree[follow], heap.tree[left_child]) < 0) {
+                follow = left_child;
             }
 
-            if (heap.size > right_child && heap.compareFn(heap.tree[follow], heap.tree[right_child]) > 0) {
+            if (heap.size > right_child && heap.compareFn(heap.tree[follow], heap.tree[right_child]) < 0) {
                 follow = right_child;
             }
 
@@ -42,34 +74,60 @@ module.exports = function () {
                 var temp = heap.tree[follow];
                 heap.tree[follow] = heap.tree[index];
                 heap.tree[index] = temp;
-                sift_down(heap, follow);
+                heap.siftDown(heap, follow);
+            }
+
+        },
+
+        siftUp : function (heap, index) {
+            var parent = parentIndex(index);
+            var follow = index;
+
+            if (index !== 0 && heap.compareFn(heap.tree[follow], heap.tree[parent]) > 0) {
+                follow = parent;
+            }
+
+            if (follow !== index) {
+                var temp = heap.tree[follow];
+                heap.tree[follow] = heap.tree[index];
+                heap.tree[index] = temp;
+                heap.siftUp(heap, follow);
+            }
+        },
+
+        sort : function () {
+            var arr = [];
+            var min = this.extractMin();
+            while(min !== null) {
+                arr.push(min)
+                min = this.extractMin();
+            }
+            return arr;
+        },
+
+        buildHeap (tree) {
+            this.tree = tree;
+            this.size = tree.length;
+            for (var i = Math.floor(tree.length/2); i >= 0; i--) {
+                this.siftDown(this, i);
             }
         }
     }
 
-    function siftUp (heap, index) {
-
-    }
-
-
 
     function parentIndex (currentIndex) {
-        return (currentIndex)/2;
+        return Math.floor(currentIndex/2);
     }
 
-    function rightIndex(currentIndex) {
-        return 2*currentIndex + 1;
+    function rightChildIndex (currentIndex) {
+        return 2*currentIndex + 2;
     }
 
-    function leftIndex(currentIndex) {
-        return 2*(currentIndex - 1) + 1
+    function leftChildIndex (currentIndex) {
+        return 2*currentIndex + 1
     }
 
-    function buildHeap (tree) {
-        for (var i = Math.floor(tree.length/2); i >= 1; i--) {
-            sift_down(tree, i);
-        }
-    }
+
 
     return BinaryHeap
 }()
--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 
 var BinaryHeap = require('./binaryheap.js')
 
-binheap = new BinaryHeap();
+var heap = new BinaryHeap([], (x,y) => x-y);
 
+heap.insert(4);
+heap.insert(1);
+heap.insert(3);
+heap.insert(2);
+heap.insert(-1);
 
+console.log(heap.tree)
+console.log(heap.sort());
+
+var h = new BinaryHeap([3,2,-1,1,2,4]);
+
+console.log(h.tree)
+console.log(h.sort())
